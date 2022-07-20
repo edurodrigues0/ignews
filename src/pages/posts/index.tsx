a52db7a18368b75c627ec7b82993b75913a2987a
edurodrigues0 <edusacramento9@gmail.com>
2022-07-20 13:50:22 -0300

    Adding unit test of the posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,10 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
-
 import { getPrismicClient } from '../../services/prismic';
-
 import styles from './styles.module.scss';
-import Link from 'next/link';
 
 type Post = {
     slug: string;
@@ -28,8 +26,8 @@ export default function Posts({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     { posts.map(post => (
-                        <Link href={`/posts/${post.slug}`}>
-                            <a key={ post.slug }>
+                        <Link key={ post.slug } href={`/posts/${post.slug}`}>
+                            <a>
                                 <time> { post.updatedAt }</time>
                                 <strong>{ post.title }</strong>
                                 <p>{ post.excerpt }</p>
